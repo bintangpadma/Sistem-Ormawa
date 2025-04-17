67129a8979e079b336eecefccb27703e4947f44b
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-04-17 15:32:22 +0800

    Progress: Minified class dashboard page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,39 +1,39 @@
 @extends('template.dashboard')
 
 @section('content')
-    <div class="content-menu grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[12px] md:gap-[16px] lg:gap-[20px]">
-        <div class="menu-dashboard">
-            <div class="dashboard-icon">
+    <div class="content-menu content-dashboard">
+        <div class="dashboard-menu">
+            <div class="menu-icon">
                 <span class="bg-dashboard-student-organization-dark"></span>
             </div>
-            <div class="dashboard-data">
+            <div class="menu-data">
                 <h6 class="data-name">Total Ormawa</h6>
                 <p class="data-value">{{ $studentOrganizationCount }}</p>
             </div>
         </div>
-        <div class="menu-dashboard">
-            <div class="dashboard-icon">
+        <div class="dashboard-menu">
+            <div class="menu-icon">
                 <span class="bg-dashboard-student-organization-dark"></span>
             </div>
-            <div class="dashboard-data">
+            <div class="menu-data">
                 <h6 class="data-name">Total UKM</h6>
                 <p class="data-value">{{ $studentActivityUnitCount }}</p>
             </div>
         </div>
-        <div class="menu-dashboard">
-            <div class="dashboard-icon">
+        <div class="dashboard-menu">
+            <div class="menu-icon">
                 <span class="bg-dashboard-student-organization-dark"></span>
             </div>
-            <div class="dashboard-data">
+            <div class="menu-data">
                 <h6 class="data-name">Total Berita</h6>
                 <p class="data-value">{{ $newsCount }}</p>
             </div>
         </div>
-        <div class="menu-dashboard">
-            <div class="dashboard-icon">
+        <div class="dashboard-menu">
+            <div class="menu-icon">
                 <span class="bg-dashboard-student-organization-dark"></span>
             </div>
-            <div class="dashboard-data">
+            <div class="menu-data">
                 <h6 class="data-name">Total Event</h6>
                 <p class="data-value">{{ $eventCount }}</p>
             </div>
