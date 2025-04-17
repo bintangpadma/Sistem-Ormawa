e7f1f2d1eb82ff14cfe7e6e7b1d330feb9a7e837
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-04-17 15:45:49 +0800

    Progress: Minified class on edit admin page

--- resources/views/dashboard/admin/edit.blade.php
+++ resources/views/dashboard/admin/edit.blade.php
@@ -1,18 +1,18 @@
 @extends('template.dashboard')
 
 @section('content')
-    <div class="content-menu p-[16px] lg:p-[20px] border border-light/[0.06] rounded-[4px]">
-        <form action="{{ route('admin.update', $admin) }}" method="POST" class="grid grid-cols-1 lg:grid-cols-2 gap-[12px] w-full" enctype="multipart/form-data">
+    <div class="content-menu content-table">
+        <form action="{{ route('admin.update', $admin) }}" method="POST" class="form lg:!grid-cols-2" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="form-input lg:col-span-2">
-                <label for="profile_path">
+                <label>
                     Foto Admin
-                    <div class="wrapper flex items-end gap-2 mt-[8px]">
-                        <img src="{{ $admin->profile_path ? asset('assets/image/admin/' . $admin->profile_path) : 'https://placehold.co/100?text=Image+Not+Found' }}" alt="Image Not Found" class="img-preview w-[100px] h-[100px] object-cover aspect-square rounded-[4px]">
-                        <input type="file" class="absolute opacity-0 top-0 left-0 input-file" id="profile_path" name="profile_path">
-                        <div class="button-secondary cursor-pointer text-[0.875rem] py-[12px] px-[16px]">Pilih foto</div>
-                    </div>
+                    <span class="input-image">
+                        <img src="{{ $admin->profile_path ? asset('assets/image/admin/' . $admin->profile_path) : 'https://placehold.co/100?text=Image+Not+Found' }}" alt="Image Not Found" class="image-preview">
+                        <input type="file" class="image-input-hidden" id="profile_path" name="profile_path">
+                        <div class="button-secondary image-button">Pilih foto</div>
+                    </span>
                 </label>
                 @error('profile_path')
                 <p class="text-invalid">{{ $message }}</p>
@@ -41,7 +41,7 @@
             </div>
             <div class="form-input">
                 <label for="password">Password</label>
-                <input type="password" class="input" name="password" placeholder="Masukkan password admin...">
+                <input type="password" class="input" name="password" placeholder="Ganti password admin...">
                 @error('password')
                 <p class="text-invalid">{{ $message }}</p>
                 @enderror
@@ -80,7 +80,7 @@
                 <p class="text-invalid">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="button-group flex items-center gap-[8px] lg:gap-[12px] lg:col-span-2">
+            <div class="button-group">
                 <button type="submit" class="button-primary">Simpan Perubahan</button>
                 <a href="{{ route('admin.index') }}" class="button-secondary">Batal Edit</a>
             </div>
@@ -88,11 +88,11 @@
     </div>
 
     <script>
-        const tagImage = document.querySelector('.img-preview');
-        const inputImage = document.querySelector('.input-file');
+        const imagePreview = document.querySelector('.image-preview');
+        const imageInput = document.querySelector('.image-input-hidden');
 
-        inputImage.addEventListener('change', function() {
-            tagImage.src = URL.createObjectURL(inputImage.files[0]);
+        imageInput.addEventListener('change', function() {
+            imagePreview.src = URL.createObjectURL(imageInput.files[0]);
         });
     </script>
 @endsection
