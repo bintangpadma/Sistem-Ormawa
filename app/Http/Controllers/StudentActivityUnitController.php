6aff274a3974343da1167cffb71658562c90cf09
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-04-18 09:20:55 +0800

    ProgressL Finish make module student activity unit

--- app/Http/Controllers/StudentActivityUnitController.php
+++ app/Http/Controllers/StudentActivityUnitController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StudentActivityUnit;
 use Illuminate\Http\Request;
 
 class StudentActivityUnitController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+        $studentActivityUnits = StudentActivityUnit::when($search, function ($query, $search) {
+                $query->where('name', 'LIKE', '%' . $search . '%')
+                    ->orWhere('abbreviation', 'LIKE', '%' . $search . '%')
+                    ->orWhere('description', 'LIKE', '%' . $search . '%');
+            })->latest()->paginate(10);
+
+        return view('dashboard.student-activity-unit.index', [
+            'page' => 'Halaman UKM',
+            'studentActivityUnits' => $studentActivityUnits,
+            'search' => $search,
+        ]);
+    }
+
+    public function show(StudentActivityUnit $studentActivityUnit)
+    {
+        return response()->json([
+            'status_code' => 200,
+            'student_activity_unit' => $studentActivityUnit,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:100',
+                'abbreviation' => 'required|string|max:50',
+                'description' => 'required|string',
+            ]);
+            StudentActivityUnit::create($validatedData);
+            return redirect()->back()->with('success', 'Berhasil menambahkan ukm baru!');
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            return redirect()->back()->with('failed', 'Gagal menambahkan ukm baru!');
+        }
+    }
+
+    public function update(StudentActivityUnit $studentActivityUnit, Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:100',
+                'abbreviation' => 'required|string|max:50',
+                'description' => 'required|string',
+            ]);
+            $studentActivityUnit->update($validatedData);
+            return redirect()->back()->with('success', 'Berhasil mengedit ukm!');
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            return redirect()->back()->with('failed', 'Gagal mengedit ukm!');
+        }
+    }
+
+    public function destroy(StudentActivityUnit $studentActivityUnit)
+    {
+        try {
+            $studentActivityUnit->delete();
+            return redirect()->back()->with('success', 'Berhasil menghapus ukm!');
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            return redirect()->back()->with('failed', 'Gagal menghapus ukm!');
+        }
+    }
 }
---
+++ resources/views/dashboard/student-activity-unit/index.blade.php
@@ -0,0 +1,109 @@
+@extends('template.dashboard')
+
+@section('content')
+    @if (session()->has('success'))
+        <div class="alert alert-success" role="alert">
+            {{ session('success') }}
+        </div>
+    @elseif(session()->has('failed'))
+        <div class="alert alert-danger" role="alert">
+            {{ session('failed') }}
+        </div>
+    @endif
+    <div class="content-menu content-table">
+        <div class="table-header">
+            <form method="GET" class="form">
+                <input type="search" class="input" name="search" placeholder="Cari ukm..." value="{{ $search }}">
+            </form>
+            @if(auth()->user()->admin)
+                <button class="button-primary" data-target="createModal" onclick="openModal(this)">Tambah UKM</button>
+            @endif
+        </div>
+        <div class="table-group">
+            <table>
+                <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Singkatan</th>
+                    <th>Deskripsi</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                @if ($studentActivityUnits->count() == 0)
+                    <td colspan="4">Data ukm tidak ditemukan!</td>
+                @else
+                    @foreach ($studentActivityUnits as $studentActivityUnit)
+                        <tr>
+                            <td>{{ $studentActivityUnit->name }}</td>
+                            <td>{{ $studentActivityUnit->abbreviation }}</td>
+                            <td>{{ \Illuminate\Support\Str::limit($studentActivityUnit->description, 40) }}</td>
+                            <td>
+                                <div class="action-button">
+                                    <button class="button icon-detail" data-target="detailModal" data-id="{{ $studentActivityUnit->id }}" onclick="openModal(this)">
+                                        <span class="bg-detail-primary"></span>
+                                    </button>
+                                    @if(auth()->user()->admin)
+                                        <button class="button icon-edit" data-target="editModal" data-id="{{ $studentActivityUnit->id }}" onclick="openModal(this)">
+                                            <span class="bg-edit-warning"></span>
+                                        </button>
+                                        <button class="button icon-delete" data-target="deleteModal" data-id="{{ $studentActivityUnit->id }}" onclick="openModal(this)">
+                                            <span class="bg-delete-danger"></span>
+                                        </button>
+                                    @endif
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </div>
+        <div class="table-paginate">
+            {{ $studentActivityUnits->links() }}
+        </div>
+    </div>
+    @include('modal.student-activity-unit')
+
+    <script>
+        function fetchStudentActivityUnit(modal, studentActivityUnitId) {
+            fetch('/student-activity-unit/' + studentActivityUnitId, {
+                method: 'GET',
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status_code === 200) {
+                        modal.querySelector('input[name="name"]').value = data.student_activity_unit.name;
+                        modal.querySelector('input[name="abbreviation"]').value = data.student_activity_unit.abbreviation;
+                        modal.querySelector('textarea[name="description"]').value = data.student_activity_unit.description;
+                    } else {
+                        console.log('Data student activity unit not found!');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
+
+        async function openModal(element) {
+            const modalTarget = element.getAttribute('data-target')
+            const modalId = element.getAttribute('data-id')
+            const modal = document.getElementById(`${modalTarget}`)
+            modal.classList.add('show')
+            if (modalTarget.includes('create')) {
+                document.getElementById('buttonCreateStudentActivityUnit').setAttribute('action', '/student-activity-unit/')
+            } else if (modalTarget.includes('detail')) {
+                await fetchStudentActivityUnit(modal, modalId)
+            } else if (modalTarget.includes('edit')) {
+                await fetchStudentActivityUnit(modal, modalId)
+                document.getElementById('buttonEditStudentActivityUnit').setAttribute('action', '/student-activity-unit/' + modalId)
+            } else if (modalTarget.includes('delete')) {
+                document.getElementById('buttonDeleteStudentActivityUnit').setAttribute('action', '/student-activity-unit/' + modalId)
+            }
+        }
+
+        function closeModal(modalTarget) {
+            document.getElementById(`${modalTarget}`).classList.remove('show')
+        }
+    </script>
+@endsection
